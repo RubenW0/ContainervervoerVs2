7a7f4ea308dc12eefa26e7b6ade01444039414aa
RubenW0 <rubenwassenberg@gmail.com>
2025-01-16 22:02:39 +0100

    fixed mistake

--- ContainervervoerVs2/Program.cs
+++ ContainervervoerVs2/Program.cs
@@ -54,40 +54,4 @@ namespace Containervervoer
             ship.StartVisualizer();
         }
     }
-    public class Container
-    {
-        public static readonly int EmptyWeight = 4;
-        public static readonly int MaxWeight = 30;
-        public int Weight { get; private set; }
-        public Type ContainerType { get; private set; }
-
-        public enum Type
-        {
-            Normal = 1,
-            Valuable = 2,
-            Coolable = 3,
-            CoolableValuable = 4
-        }
-
-        public Container(int weight, Type containerType)
-        {
-            if (weight < EmptyWeight)
-            {
-                throw new Exception("Gewicht kan niet minder zijn dan 4 ton");
-            }
-            else if (weight > MaxWeight)
-            {
-                throw new Exception("Gewicht kan niet meer zijn dan 30 ton");
-            }
-            else
-            {
-                this.Weight = weight;
-            }
-
-            this.ContainerType = containerType;
-        }
-
-        public bool IsValuable => ContainerType == Type.Valuable || ContainerType == Type.CoolableValuable;
-        public bool NeedsCooling => ContainerType == Type.Coolable || ContainerType == Type.CoolableValuable;
-    }
 }
